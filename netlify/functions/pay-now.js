722e5d7dce3333b23f21116b3e7afc0d535d5077
aarzoo491 <aarzoo@multilinetechnolabs.com>
2026-04-03 11:43:22 +0530

    Update webhook URL in pay-now function

--- netlify/functions/pay-now.js
+++ netlify/functions/pay-now.js
@@ -12,7 +12,7 @@ exports.handler = async (event) => {
     console.log("Form Data Sent:", form.toString());
 
     const response = await axios.post(
-      "https://visioptdev.com/app/webhook_capture_custom/MTA0MC00MjMtMTMzLVc=",
+      "https://visiopt.com/app/webhook_capture_custom/MTA5Mi00MjMtMTU1LVc=",
       form,
       { headers: { "Content-Type": "application/x-www-form-urlencoded" } }
     );
