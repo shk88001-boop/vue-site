d2bfc8fbebf81c993ad9126fd2822f6cc9fb6beb
shk88001-boop <shk88001@gmail.com>
2026-06-02 16:28:40 +0530

    Add plugin to remove crossorigin attributes

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,17 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
     vueDevTools(),
+    {
+      name: 'remove-crossorigin',
+      transformIndexHtml(html) {
+        return html.replace(/\s*crossorigin(?:="")?/g, '')
+      }
+    }
   ],
   resolve: {
     alias: {
